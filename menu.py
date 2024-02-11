a71092157a01b8c1a4295fca428a54679129ebe3
david-ramallal <davidgarciaramallal@gmail.com>
2024-02-11 18:35:09 +0100

    starting with menu audio

--- menu.py
+++ menu.py
@@ -9,6 +9,7 @@ from loop import play_game
 
 # Function to start the game (placeholder)
 def start_game():
+    pygame.mixer.music.stop()
     play_game()
 
 
@@ -86,9 +87,30 @@ def write_credits(labels):
     return menu
 
 
+"""
+SI NOS QUEDAMOS ESA CANCION HAY QUE INCLUIR LO SIGUIENTE EN LOS CREDITOS:
+
+Fall From Grace by Darren Curtis | https://www.darrencurtismusic.com/
+Music promoted by https://www.chosic.com/free-music/all/
+Creative Commons CC BY 3.0
+https://creativecommons.org/licenses/by/3.0/
+
+
+
+"""
+
 # Main function
 def main():
     pygame.init()
+
+    # Inicialize the music mixer
+    pygame.mixer.init()
+
+    # Loads and reproduce music 
+    pygame.mixer.music.load('assets\Fall-From-Grace(chosic.com).mp3')
+    pygame.mixer.music.play(-1)  # -1 to infinity music
+
+
     screen = pygame.display.set_mode((800, 600))
 
     splash_screen(screen, 10)
