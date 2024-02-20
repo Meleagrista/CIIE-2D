6486dde54bee0f7c4358866ade8d28f1386e458f
yref-boop <regofdez.yago@gmail.com>
2024-02-20 19:45:14 +0100

    feat: boxed camera, instead of fixed to player

--- camera.py
+++ camera.py
@@ -1,14 +1,28 @@
 import pygame
 from utils.constants import *
 
+
 class Camera(pygame.sprite.Group):
 
     def __init__(self):
         super().__init__()
         self.surface = pygame.display.get_surface()
         self.offset = pygame.math.Vector2(0, 0)
-        self.half_width = self.surface.get_width() // 2
-        self.half_height = self.surface.get_height() // 2
+
+        # offset from screen to camera border
+        self.boundary_corners = {
+            'left': 200,
+            'right': 200,
+            'top': 200,
+            'bottom': 200
+        }
+
+        # camera boundaries
+        left_corner = self.boundary_corners['left']
+        top_corner = self.boundary_corners['top']
+        boundary_width = self.surface.get_width() - (self.boundary_corners['left'] + self.boundary_corners['right'])
+        boundary_height = self.surface.get_height() - (self.boundary_corners['top'] + self.boundary_corners['bottom'])
+        self.boundary = pygame.Rect(left_corner, top_corner, boundary_width, boundary_height)
 
     def draw_bar(self, position, width, height, percentage):
         position = position - self.offset
@@ -31,12 +45,30 @@ class Camera(pygame.sprite.Group):
     def custom_draw(self, player, grid):
 
         self.surface.fill((255, 255, 255))
-        self.offset.x = player.rect.x - self.half_width
-        self.offset.y = player.rect.y - self.half_height
+
+        # update boundary if player is outside
+        if player.rect.left < self.boundary.left:
+            self.boundary.left = player.rect.left
+        if player.rect.right > self.boundary.right:
+            self.boundary.right = player.rect.right
+        if player.rect.top < self.boundary.top:
+            self.boundary.top = player.rect.top
+        if player.rect.bottom > self.boundary.bottom:
+            self.boundary.bottom = player.rect.bottom
+
+        self.offset.x = self.boundary.left - self.boundary_corners['left']
+        self.offset.y = self.boundary.top - self.boundary_corners['top']
 
         grid.draw(self.offset)
 
         # Sort ensures grid is drawn on background
         for sprite in sorted(self.sprites(), key=lambda sprite: 0 - sprite.rect.width):
-            # offset_position = sprite.rect.topleft - self.offset
             sprite.draw(self.surface, self.offset)
+
+        # draw camera (for testing)
+        left_corner = self.boundary_corners['left']
+        top_corner = self.boundary_corners['top']
+        boundary_width = self.surface.get_width() - (self.boundary_corners['left'] + self.boundary_corners['right'])
+        boundary_height = self.surface.get_height() - (self.boundary_corners['top'] + self.boundary_corners['bottom'])
+        boundary = pygame.Rect(left_corner, top_corner, boundary_width, boundary_height)
+        pygame.draw.rect(self.surface, 'yellow', boundary, 4)
